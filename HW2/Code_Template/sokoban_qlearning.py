525129c25f00e464e65711909b4eb06127503a97
ZHITENGLI <cnbgcnbg@126.com>
2021-10-16 10:39:26 +0800

    update sokoban

--- HW2/Code_Template/sokoban_qlearning.py
+++ HW2/Code_Template/sokoban_qlearning.py
@@ -28,7 +28,8 @@ np.random.seed(RANDOM_SEED)
 ####### START CODING HERE #######
 
 # construct the intelligent agent.
-agent = QLearningAgent(all_actions)
+# add paras size of action space
+agent = QLearningAgent(all_actions, num_actions)
 
 # start training
 for episode in range(1000):
@@ -38,18 +39,44 @@ for episode in range(1000):
     env.render()
     # agent interacts with the environment
     for iter in range(500):
-        a = agent.choose_action(s)
+        # convert coordinate to index
+        idx_s0 = s[0] + 7*s[1]
+        idx_s1 = s[2] + 7*s[3]
+        idx_s2 = s[4] + 7*s[5]
+
+        # a = agent.choose_action(idx_s1)
+        a = agent.choose_action(idx_s0)
         s_, r, isdone, info = env.step(a)
+
+        # convert coordinate to index
+        idx_s_0 = s_[0] + 7*s_[1]
+        idx_s_1 = s_[2] + 7*s_[3]
+        idx_s_2 = s_[4] + 7*s_[5]
+
         env.render()
         episode_reward += r
-        print(f"{s} {a} {s_} {r} {isdone}")
-        agent.learn()
+        # print(f"{s} {a} {s_} {r} {isdone}")
+        # agent.learn(idx_s1, idx_s_1, a, r, gamma=0.9)
+        agent.learn(idx_s0, idx_s_0, a, r, gamma=0.9)
         s = s_
         if isdone:
-            time.sleep(0.5)
+            # # assign the value of teminal
+            # for a in all_actions:
+            #     agent.q_table[idx_s1][a] = r
+
+            # time.sleep(0.5)
             break
 
     print('episode:', episode, 'episode_reward:', episode_reward, 'epsilon:', agent.epsilon)  
+
+    # decrease epsilon and learning rate
+    if (agent.epsilon < 1e-2):
+        agent.epsilon = 0
+    else:
+        agent.epsilon *= 0.99
+
+    agent.lr *= 0.99
+
 print('\ntraining over\n')   
 
 # close the render window after training.
