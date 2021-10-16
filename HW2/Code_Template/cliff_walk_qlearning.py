56a0d8c3672b350b8dd4b4af3a96ad88046cd36f
ZHITENGLI <cnbgcnbg@126.com>
2021-10-16 10:49:30 +0800

    update Q-learning

--- HW2/Code_Template/cliff_walk_qlearning.py
+++ HW2/Code_Template/cliff_walk_qlearning.py
@@ -68,16 +68,16 @@ for episode in range(1000):
     if (agent.epsilon < 1e-2):
         agent.epsilon = 0
     else:
-        agent.epsilon *= 0.95
+        agent.epsilon *= 0.9
     agent.lr *= 0.99
 
 
     # At last, we should plot the episode reward during the train process.
     # We use excel to plot it because this may be seen nicely.
-    with open(r'HW2\\Code_Template\\episode_reward_Q_learning.txt', 'a', encoding='utf-8') as f:
+    with open(r'd:\Users\86134\Documents\github\AI3603_HW\AI3603_HW\HW2\Code_Template\episode_reward_Q_learning.txt', 'a', encoding='utf-8') as f:
         f.write(str(episode) + '\t' + str(episode_reward) + '\n')
     # Plot the epsilon value during the training.
-    with open(r'HW2\\Code_Template\\epsilon_value_Q_learning.txt', 'a', encoding='utf-8') as f:
+    with open(r'd:\Users\86134\Documents\github\AI3603_HW\AI3603_HW\HW2\Code_Template\epsilon_value_Q_learning.txt', 'a', encoding='utf-8') as f:
         f.write(str(episode) + '\t' + str(agent.epsilon) + '\n')
 
 # print('\ntraining over\n')   
